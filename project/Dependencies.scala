d6aa8cfa7a3ab8eca6434a2a79a0b3aa7748e270
MugishaU <muwiragiye123@gmail.com>
2021-09-26 14:03:20 +0100

    Add dynamodb dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,12 +2,16 @@
 import sbt._
 
 object Dependencies {
-  val awsVersion = "2.17.42"
+
+  val awsJDKVersion = "1.12.70"
+
   lazy val projectDependencies = Seq(
-    "software.amazon.awssdk" % "dynamodb" % awsVersion,
+    "com.amazonaws" % "aws-java-sdk-dynamodb" % awsJDKVersion,
+    "com.amazonaws" % "aws-java-sdk-s3" % awsJDKVersion,
     "com.amazonaws" % "aws-lambda-java-core" % "1.2.1",
     "com.amazonaws" % "aws-lambda-java-events" % "3.10.0"
   )
+
   lazy val testDependencies = Seq(
     "org.scalatest" %% "scalatest" % "3.3.0-SNAP3" % Test
   )
